c249b4b8fbac13e0a6565d18fa3e7fb2773237b2
gmcnutt <gmcnutt@purestorage.com>
2021-09-13 22:09:19 +0000

    initial checkin -- re-generated from old github.com/llaaiiqq/go-nfs-client v0.0.0-20190621174953-b1983f0e2bb0

---
+++ README.md
@@ -0,0 +1,8 @@
+NFS Client for Golang
+=====================
+
+[![godoc](http://godoc.org/github.com/fdawg4l/nfs?status.svg)](http://godoc.org/github.com/fdawg4l/nfs)
+
+This is forked from and builds upon the work done [here](https://github.com/davecheney/nfs) which was put in the [public domain](https://github.com/davecheney/nfs/issues/1#issuecomment-280563247).
+
+The objective is to build a minimal NFSv3 client.
---
+++ nfs/nfs_test.go
@@ -0,0 +1,57 @@
+// Copyright © 2017 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: BSD-2-Clause
+//
+package nfs
+
+import (
+	"fmt"
+	"net"
+	"sync"
+	"testing"
+)
+
+func listenAndServe(t *testing.T, port int) (*net.TCPListener, *sync.WaitGroup, error) {
+
+	addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("127.0.0.1:%d", port))
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return nil, nil, err
+	}
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		l.Accept()
+		t.Logf("Accepted conn")
+		l.Accept()
+		t.Logf("Accepted conn")
+		wg.Done()
+	}()
+
+	return l, wg, nil
+}
+
+// test we can bind without colliding
+func TestDialService(t *testing.T) {
+	listener, wg, err := listenAndServe(t, 6666)
+	if err != nil {
+		t.Logf("error starting listener: %s", err.Error())
+		t.Fail()
+		return
+	}
+	defer listener.Close()
+
+	_, err = dialService("127.0.0.1", 6666)
+	if err != nil {
+		t.Logf("error dialing: %s", err.Error())
+		t.FailNow()
+	}
+
+	_, err = dialService("127.0.0.1", 6666)
+	if err != nil {
+		t.Logf("error dialing: %s", err.Error())
+		t.FailNow()
+	}
+
+	wg.Wait()
+}
